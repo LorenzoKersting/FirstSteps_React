8d0d5da7bb5d13aec9f25843765c3213373d3d8c
LorenzoKersting <53866766+LorenzoKersting@users.noreply.github.com>
2020-07-23 14:27:02 -0300

    Add files via upload
    Captura de dados de uma API + visualização dos dados em formato de lista e design da página + navegação entre diferentes páginas.

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from   'react';
+
+import "./style.css"
+import App from '../../App';
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
